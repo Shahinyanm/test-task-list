cf55d79001c41bf1c91d36dc0bcd98abbe95587c
shahinyanm <Shahinyanm@gmail.com>
2022-08-25 14:02:46 +0400

    Init app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class="w-max-content mx-auto h-screen py-12 ">
+    <div class="w-full flex items-center justify-center">
+      <div class="text-main text-[36px] font-medium"> Some text here</div>
+    </div>
+    <div class="w-full flex items-center justify-start">
+      <div class="w-full flex items-center justify-start gap-4">
+        <div class="text-black text-[16px] font-medium">Filter</div>
+        <div class="flex items-center justify-start">
+          <input  type="text" class="rounded-full p-1 border-1 border-black">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],  theme: {
+    extend: {
+      colors:{
+        'main':'#45852D'
+      },
+      maxWidth:{
+        content:"1440px"
+      },
+      width:{
+        '36p':'36px'
+      }
+    },
+  },
+  plugins: [],
+}
